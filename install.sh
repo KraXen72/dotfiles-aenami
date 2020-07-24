0e18a36d373003cd1b98b0a896d068a7149de696
rajshekhar26 <raj.shekhar2622@gmail.com>
2020-07-24 21:23:29 +0530

    remove script

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'alias config="/usr/bin/git --git-dir=$HOME/Projects/dotfiles --work-tree=$HOME"' >> $HOME/.zshrc
-source ~/.zshrc
-echo "~/Projects/Dotfiles" >> .gitignore
-mkdir ~/Projects
-git clone --bare https://www.github.com/rajshekhar26/dotfiles $HOME/Projects/dotfiles
-config checkout
-config config --local status.showUntrackedFiles no
